7260acda8f702ef43236dbda78a28426ecc204b1
AlmedinShala <almedinsh22@hotmail.com>
2022-10-09 11:58:12 +0200

    Commit initial files

---
+++ index.js
@@ -0,0 +1,55 @@
+let homeScoreBtnOne = document.getElementById("home-score-btn-1")
+let homeStoreEl = document.getElementById("home-score")
+let homeScore = 0
+
+function almedin(){
+    homeScore += 1
+    homeStoreEl.textContent = homeScore
+}
+
+let homeScoreBtnTwo = document.getElementById("home-score-btn-2")
+let homeStoreEl2 = document.getElementById("home-score")
+let homeScore2 = 0
+
+
+function almedin1(){
+    homeScore2 += 2
+    homeStoreEl2.textContent = homeScore2 
+   
+}
+
+let homeScoreBtnThree = document.getElementById("home-score-btn-3")
+let homeStoreEl3 = document.getElementById("home-score")
+let homeScore3 = 0
+
+
+function almedin2(){
+    homeScore3 += 3
+    homeStoreEl3.textContent = homeScore3 
+   
+}
+let asd1 = document.getElementById("home-score-btn-1")
+let asd2 = document.getElementById("home-score-btn-2")
+let asd3 = document.getElementById("home-score-btn-3")
+let scoreTotalEl = document.getElementById("home-score")
+let scoreTotal = 0
+
+function asdAll(){
+   
+    scoreTotalEl.textContent = scoreTotal
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
